8ba11764973a68314717213a74ec5f59230646d9
mzhouURI <mzhou@uri.edu>
2025-01-18 15:25:35 -0500

    setup sim actuators

---
+++ race2_auv_bringup/launch/bringup_simulation.launch.py
@@ -0,0 +1,27 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PythonExpression
+import time
+
+
+
+
+def generate_launch_description():
+    arg_robot_name = 'race2_auv'
+    robot_bringup = arg_robot_name + '_bringup'
+
+    # simulation
+    simulation = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(get_package_share_directory(robot_bringup), 'launch','include','simulation.launch.py')]),
+        launch_arguments = {'arg_robot_name': arg_robot_name}.items()    
+    )
+
+
+    return LaunchDescription([
+        simulation,
+    ])
