892d0db3975e4327ec2c438c69c382139989455b
mzhouURI <mzhou@uri.edu>
2025-01-30 15:44:12 -0500

    race2 thruster config change

--- race2_auv_bringup/launch/include/description.launch.py
+++ race2_auv_bringup/launch/include/description.launch.py
@@ -26,12 +26,12 @@ def generate_launch_description():
 
 
     return LaunchDescription([
-        Node(
-            package='rviz2',
-            executable='rviz2',
-            name='rviz2',
-            # arguments=['-d', [rviz_config_dir]],
-        ),
+        # Node(
+        #     package='rviz2',
+        #     executable='rviz2',
+        #     name='rviz2',
+        #     # arguments=['-d', [rviz_config_dir]],
+        # ),
         
         Node(
             package='robot_state_publisher',
